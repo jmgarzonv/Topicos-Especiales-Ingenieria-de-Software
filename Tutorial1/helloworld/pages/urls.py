ce39249b85173e93a3ea9a2c039257897da00fac
jmgarzonv <68928349+jmgarzonv@users.noreply.github.com>
2025-02-13 17:58:08 -0500

    Add files via upload

---
+++ Tutorial1/helloworld/pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView, ProductCreateView, ProductIndexView, ProductShowView
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create/', ProductCreateView.as_view(), name='form'),  # Nueva ruta
+    path('products/<str:id>/', ProductShowView.as_view(), name='show'),
+]
